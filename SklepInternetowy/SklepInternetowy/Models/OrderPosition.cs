88e7706cde4143d38df71776088e7c4dce51e4ce
Mateusz Kawka <matiz21@op.pl>
2017-08-26 22:09:04 +0200

    OrderPositionEdit

--- SklepInternetowy/SklepInternetowy/Models/OrderPosition.cs
+++ SklepInternetowy/SklepInternetowy/Models/OrderPosition.cs
@@ -6,7 +6,7 @@
         public int OrderId { get; set; }
         public int SupplementId { get; set; }
         public int Amount { get; set; }
-        public decimal PurchasePrice { get; set; }
+        //public decimal PurchasePrice { get; set; }
 
         public virtual Supplement Supplement { get; set; }
         public virtual Order Order { get; set; }
