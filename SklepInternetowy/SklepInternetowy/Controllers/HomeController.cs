68b12da0f9e4d1a5fb812b4454a69fe3682ac7fe
Mateusz Kawka <matiz21@op.pl>
2017-08-26 23:25:49 +0200

    First entry in the database

--- SklepInternetowy/SklepInternetowy/Controllers/HomeController.cs
+++ SklepInternetowy/SklepInternetowy/Controllers/HomeController.cs
@@ -1,17 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using SklepInternetowy.Data_Access_Layer;
+using SklepInternetowy.Models;
 using System.Web.Mvc;
 
 namespace SklepInternetowy.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
-        //komentarz
+        private SupplementsContext db = new SupplementsContext();
+
+
         public ActionResult Index()
         {
+            Category category = new Category()
+            {
+                CategoryName = "Witaminy i minerały",
+                IconFileName = "Witaminy.png",
+                CategoryDescription = "Najważniejsze elementy zapewniające zdrowie, energię i witalność"
+            };
+
+            db.Categories.Add(category);
+            db.SaveChanges();
             return View();
         }
     }
