cf0a11ba87ee1e60b314f6fda3d4648554874a7a
Mateusz Kawka <matiz21@op.pl>
2017-08-26 23:08:04 +0200

    Added SupplementsContext

---
+++ SklepInternetowy/SklepInternetowy/Data Access Layer/SupplementsContext.cs
@@ -0,0 +1,29 @@
+﻿using SklepInternetowy.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace SklepInternetowy.Data_Access_Layer
+{
+    public class SupplementsContext : DbContext
+    {
+        public SupplementsContext() : base("SupplementsContext")
+        {
+        }
+
+        public DbSet<Supplement> Supplements { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderPosition> OrderPositions { get; set; }
+
+
+        //usunięcie opcji automatycznego dopisywania S na końcu nazw tabel
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
